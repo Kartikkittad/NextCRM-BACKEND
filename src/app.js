8fb69ed1700f86f0cdcc4e70055cb0edd963af0e
Kartikkittad <kartikkittad85@gmail.com>
2026-01-15 15:09:38 +0530

    fix: :bug: swagger ui bugs fixes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,21 @@
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerSpec from "./utils/swagger.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import leadRoutes from "./routes/lead.routes.js";
-import swaggerUi from "swagger-ui-express";
-
-const swaggerUiAssets = swaggerUi.serveFiles(swaggerSpec);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/leads", leadRoutes);
-
-app.use("/api/docs", swaggerUiAssets, swaggerUi.setup(swaggerSpec));
 
 export default app;
